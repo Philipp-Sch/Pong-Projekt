7f3ec34c7f5b5d22b4f4d2cc4a41eae8baf429d9
Philipp Schiffbauer <freackyphil@gmail.com>
2017-05-24 09:02:45 +0200

    Revert "Kaka"
    This reverts commit 6bb6edfd6d544e41befc2e3350c20946ae13d469.

--- Pong 0.1/Pong 0.1/Form1.Designer.cs
+++ Pong 0.1/Pong 0.1/Form1.Designer.cs
@@ -28,18 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(984, 561);
-            this.Name = "Form1";
             this.Text = "Form1";
-            this.ResumeLayout(false);
-
         }
 
         #endregion
