03fde17afd764ae858ec872f19898e679f252e8f
semen <semen@DESKTOP-AE205SL>
2017-06-07 08:58:41 +0200

    nicht mergen !

--- Pong 0.1/Pong 0.1/GameForms.cs
+++ Pong 0.1/Pong 0.1/GameForms.cs
@@ -48,10 +48,11 @@ namespace Pong_0._1
         }
 
         protected override void OnPaint(PaintEventArgs e)
-        {
-            e.Graphics.FillRectangle(Brushes.White, new Rectangle(10,20 , 50, 100));
-            e.Graphics.FillRectangle(Brushes.White, new Rectangle(100, 20, 50, 100));
+        {//
+            e.Graphics.FillRectangle(Brushes.White, new Rectangle(5,20 , 50, 100));
+            e.Graphics.FillRectangle(Brushes.White, new Rectangle(680, 20, 50, 100));
             e.Graphics.FillEllipse(Brushes.Yellow, new Rectangle(Width / 2 - 100, Height / 2 - 100, 50, 50));
+
 
             base.OnPaint(e);
         }
