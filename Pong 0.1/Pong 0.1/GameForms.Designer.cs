81e0ff0eb70cce93067a8ae5ccd23ecd68bc320d
Philipp Schiffbauer <freackyphil@gmail.com>
2017-06-07 09:16:03 +0200

    Label für die Punktzahl hinzugefügt

--- Pong 0.1/Pong 0.1/GameForms.Designer.cs
+++ Pong 0.1/Pong 0.1/GameForms.Designer.cs
@@ -28,7 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.PlayerAPunkte = new System.Windows.Forms.Label();
+            this.PlayerBPunkte = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // PlayerAPunkte
+            // 
+            this.PlayerAPunkte.AutoSize = true;
+            this.PlayerAPunkte.ForeColor = System.Drawing.Color.White;
+            this.PlayerAPunkte.Location = new System.Drawing.Point(12, 9);
+            this.PlayerAPunkte.Name = "PlayerAPunkte";
+            this.PlayerAPunkte.Size = new System.Drawing.Size(83, 13);
+            this.PlayerAPunkte.TabIndex = 0;
+            this.PlayerAPunkte.Text = "Player A Punkte";
+            // 
+            // PlayerBPunkte
+            // 
+            this.PlayerBPunkte.AutoSize = true;
+            this.PlayerBPunkte.ForeColor = System.Drawing.Color.White;
+            this.PlayerBPunkte.Location = new System.Drawing.Point(639, 9);
+            this.PlayerBPunkte.Name = "PlayerBPunkte";
+            this.PlayerBPunkte.Size = new System.Drawing.Size(83, 13);
+            this.PlayerBPunkte.TabIndex = 1;
+            this.PlayerBPunkte.Text = "Player B Punkte";
             // 
             // GameForms
             // 
@@ -36,15 +58,21 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(734, 461);
+            this.Controls.Add(this.PlayerBPunkte);
+            this.Controls.Add(this.PlayerAPunkte);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "GameForms";
             this.Text = "GameForms";
             this.Load += new System.EventHandler(this.GameForms_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label PlayerAPunkte;
+        private System.Windows.Forms.Label PlayerBPunkte;
     }
 }
