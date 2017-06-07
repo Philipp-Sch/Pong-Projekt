9e12f61876d6e1c6b083fe4ce682b7b6cb6cac47
semen <semen@DESKTOP-AE205SL>
2017-06-07 08:17:28 +0200

    rectangle 1

--- Pong 0.1/Pong 0.1/Form1.Designer.cs
+++ Pong 0.1/Pong 0.1/Form1.Designer.cs
@@ -39,10 +39,9 @@
             this.PlayLabel.BackColor = System.Drawing.Color.Transparent;
             this.PlayLabel.Font = new System.Drawing.Font("Niagara Solid", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PlayLabel.ForeColor = System.Drawing.Color.White;
-            this.PlayLabel.Location = new System.Drawing.Point(330, 125);
-            this.PlayLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.PlayLabel.Location = new System.Drawing.Point(440, 154);
             this.PlayLabel.Name = "PlayLabel";
-            this.PlayLabel.Size = new System.Drawing.Size(71, 51);
+            this.PlayLabel.Size = new System.Drawing.Size(92, 64);
             this.PlayLabel.TabIndex = 0;
             this.PlayLabel.Text = "Play";
             this.PlayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -56,9 +55,10 @@
             this.SettingsLabel.AutoSize = true;
             this.SettingsLabel.Font = new System.Drawing.Font("Niagara Engraved", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SettingsLabel.ForeColor = System.Drawing.Color.White;
-            this.SettingsLabel.Location = new System.Drawing.Point(330, 209);
+            this.SettingsLabel.Location = new System.Drawing.Point(440, 257);
+            this.SettingsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.SettingsLabel.Name = "SettingsLabel";
-            this.SettingsLabel.Size = new System.Drawing.Size(118, 51);
+            this.SettingsLabel.Size = new System.Drawing.Size(146, 64);
             this.SettingsLabel.TabIndex = 1;
             this.SettingsLabel.Text = "Settings";
             this.SettingsLabel.MouseEnter += new System.EventHandler(this.SettingsLabel_MouseEnter);
@@ -69,10 +69,9 @@
             this.ExitLabel.AutoSize = true;
             this.ExitLabel.Font = new System.Drawing.Font("Niagara Engraved", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ExitLabel.ForeColor = System.Drawing.Color.White;
-            this.ExitLabel.Location = new System.Drawing.Point(336, 288);
-            this.ExitLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.ExitLabel.Location = new System.Drawing.Point(448, 354);
             this.ExitLabel.Name = "ExitLabel";
-            this.ExitLabel.Size = new System.Drawing.Size(67, 51);
+            this.ExitLabel.Size = new System.Drawing.Size(84, 64);
             this.ExitLabel.TabIndex = 2;
             this.ExitLabel.Text = "Exit";
             this.ExitLabel.Click += new System.EventHandler(this.ExitLabel_Click);
@@ -81,17 +80,17 @@
             // 
             // Pong
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(734, 461);
+            this.ClientSize = new System.Drawing.Size(979, 567);
             this.Controls.Add(this.ExitLabel);
             this.Controls.Add(this.SettingsLabel);
             this.Controls.Add(this.PlayLabel);
             this.Cursor = System.Windows.Forms.Cursors.Default;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Pong";
-            this.Text = "PONG";
+            this.Text = "s";
             this.Load += new System.EventHandler(this.Pong_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
--- Pong 0.1/Pong 0.1/GameForms.cs
+++ Pong 0.1/Pong 0.1/GameForms.cs
@@ -16,8 +16,11 @@ namespace Pong_0._1
         {
             InitializeComponent();
         }
+
+       
         protected override void OnPaint(PaintEventArgs e)
         {
+            e.Graphics.FillRectangle(Brushes.White, new Rectangle(10,20 , 50, 100));
             base.OnPaint(e);
         }
         private void GameForms_Load(object sender, EventArgs e)
