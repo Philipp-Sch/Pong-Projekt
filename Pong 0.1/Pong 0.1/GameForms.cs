0959f50c374682ea14dad769f902aa3bbc141049
NilsLaptop-PC\NilsLaptop <nilsvanzantvoort99@gmx.de>
2017-06-07 08:09:03 +0200

    GameForm Timer

--- Pong 0.1/Pong 0.1/GameForms.cs
+++ Pong 0.1/Pong 0.1/GameForms.cs
@@ -12,14 +12,27 @@ namespace Pong_0._1
 {
     public partial class GameForms : Form
     {
+        Timer myTimer;
+
         public GameForms()
         {
             InitializeComponent();
+            myTimer = new Timer();
+            myTimer.Tick += new EventHandler(TimerEventProcessor);
+            myTimer.Interval = 50;
         }
+
+        private void TimerEventProcessor(Object myObject, EventArgs myEventArgs)
+        {
+            myTimer.Stop();
+            myTimer.Start();
+        }
+
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
         }
+
         private void GameForms_Load(object sender, EventArgs e)
         {
 
