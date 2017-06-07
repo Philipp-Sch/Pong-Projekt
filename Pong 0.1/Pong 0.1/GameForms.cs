111ec8d887bcc0a8971ef0557f2fa59b25afd2aa
semen <semen@DESKTOP-AE205SL>
2017-06-07 08:27:36 +0200

    rect and ellipse

--- Pong 0.1/Pong 0.1/GameForms.cs
+++ Pong 0.1/Pong 0.1/GameForms.cs
@@ -21,6 +21,9 @@ namespace Pong_0._1
         protected override void OnPaint(PaintEventArgs e)
         {
             e.Graphics.FillRectangle(Brushes.White, new Rectangle(10,20 , 50, 100));
+            e.Graphics.FillRectangle(Brushes.White, new Rectangle(100, 20, 50, 100));
+            e.Graphics.FillEllipse(Brushes.Yellow, new Rectangle(Width / 2 - 100, Height / 2 - 100, 50, 50));
+
             base.OnPaint(e);
         }
         private void GameForms_Load(object sender, EventArgs e)
