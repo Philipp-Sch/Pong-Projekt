ee82b54714bb7bb3fdc31dfabc86bca40a1c31dc
321RORO123 <nilsvanzantvoort99@gmx.de>
2017-06-28 13:37:12 +0200

    Punkt Strich Collsion hinzugefügt

--- Pong 0.1/Pong 0.1/Collider.cs
+++ Pong 0.1/Pong 0.1/Collider.cs
@@ -66,5 +66,74 @@ namespace Pong_0._1
             else
                 return false ;
         }
+
+        public bool PunktStrichHorCollision(int xa,int ya,int xb1,int xb2,int yb)
+        {
+            //Punktkoordinaten
+            XA1 = xa;
+            YA1 = ya;
+            //Strichkoordinaten
+            XB1 = xb1;
+            XB2 = xb2;
+            YB1 = yb;
+
+            if(XB1 <= XA1 && XB2 >= XA1)
+                CollisionX = true;
+            else
+                CollisionY = false;
+
+            if (YA1 == YB1)
+                CollisionY = true;
+            else
+                CollisionY = false;
+
+            if (CollisionX == true && CollisionY == true)
+                return true;
+            else
+                return false;
+        }//Horizontal
+
+        public bool PunktStrichVerCollision(int xa, int ya, int xb, int yb1, int yb2)
+        {
+            //Punktkoordinaten
+            XA1 = xa;
+            YA1 = ya;
+            //Strichkoordinaten
+            XB1 = xb;
+            YB1 = yb1;
+            YB2 = yb2;
+
+            if (XB1 == XA1)
+                CollisionX = true;
+            else
+                CollisionY = false;
+
+            if (YB1 <= YA1 && YB2 >= YA1)
+                CollisionY = true;
+            else
+                CollisionY = false;
+
+            if (CollisionX == true && CollisionY == true)
+                return true;
+            else
+                return false;
+        }//Vertiakal
+
+        public bool PunktQuadratCollision(int xa,int ya,int xb1, int xb2, int yb1,int yb2)
+        {
+            //Punkt-Koordinaten
+            XA1 = xa;
+            YA1 = ya;
+            //Kantenwerte Quadrates 
+            XB1 = xb1;//Links
+            XB2 = xb2;//Rechts
+            YB1 = yb1;//Oben
+            YB2 = yb2;//Unten
+
+            if (CollisionX == true && CollisionY == true)
+                return true;
+            else
+                return false;
+        }
     }
 }
