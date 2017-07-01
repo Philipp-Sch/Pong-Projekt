ff121435d6108c1d6fadf0e31556c4759418a7fe
aknbarisxx <m.akinbaris@gmail.com>
2017-07-01 21:01:45 +0200

    Einstellungen hinzugefügt und alle Rechner haben gleiche Fonts!

---
+++ Pong 0.1/Pong 0.1/FontManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pong_0._1
+{
+    public class FontManager
+    {
+        private PrivateFontCollection fontCollection;
+
+        public FontManager()
+        {
+            fontCollection = new PrivateFontCollection(); //Erstellen ein FontCollection
+            byte[] fontData = Properties.Resources.NIAGSOL; //Font aus den Resources
+            IntPtr fontPtr = Marshal.AllocCoTaskMem(fontData.Length); //Pointer Adresse für die FontData erstellen
+            Marshal.Copy(fontData, 0, fontPtr, fontData.Length); //FontData in den RAM laden
+            uint dummy = 0;
+            fontCollection.AddMemoryFont(fontPtr, fontData.Length); //Font zur Collection über Pointer Adresse hinzufügen
+            AddFontMemResourceEx(fontPtr, (uint)fontData.Length, IntPtr.Zero, ref dummy);
+            Marshal.FreeCoTaskMem(fontPtr);
+        }
+
+        public Font CreateFont(float size)
+        {
+            return new Font(fontCollection.Families[0], size);
+        }
+
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr AddFontMemResourceEx(IntPtr pbFont, uint cbFont, IntPtr pdv, [In]ref uint pcFonts);
+    }
+}
--- Pong 0.1/Pong 0.1/Form1.Designer.cs
+++ Pong 0.1/Pong 0.1/Form1.Designer.cs
@@ -32,6 +32,19 @@
             this.SettingsLabel = new System.Windows.Forms.Label();
             this.ExitLabel = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.settingsPanel = new System.Windows.Forms.Panel();
+            this.rounds2Label = new System.Windows.Forms.Label();
+            this.roundsLabel = new System.Windows.Forms.Label();
+            this.roundsTrackBar = new System.Windows.Forms.TrackBar();
+            this.cancelLabel = new System.Windows.Forms.Label();
+            this.saveLabel = new System.Windows.Forms.Label();
+            this.settings2Label = new System.Windows.Forms.Label();
+            this.goals2Label = new System.Windows.Forms.Label();
+            this.goalsLabel = new System.Windows.Forms.Label();
+            this.goalsTrackBar = new System.Windows.Forms.TrackBar();
+            this.settingsPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.roundsTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.goalsTrackBar)).BeginInit();
             this.SuspendLayout();
             // 
             // PlayLabel
@@ -51,7 +64,13 @@
             this.PlayLabel.Click += new System.EventHandler(this.label1_Click);
             this.PlayLabel.MouseEnter += new System.EventHandler(this.PlayLabel_MouseEnter);
             this.PlayLabel.MouseLeave += new System.EventHandler(this.PlayLabel_MouseLeave);
-            
+            // 
+            // SettingsLabel
+            // 
+            this.SettingsLabel.Location = new System.Drawing.Point(0, 0);
+            this.SettingsLabel.Name = "SettingsLabel";
+            this.SettingsLabel.Size = new System.Drawing.Size(100, 23);
+            this.SettingsLabel.TabIndex = 4;
             // 
             // ExitLabel
             // 
@@ -79,12 +98,134 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "PONG";
             // 
+            // settingsPanel
+            // 
+            this.settingsPanel.Controls.Add(this.goals2Label);
+            this.settingsPanel.Controls.Add(this.goalsLabel);
+            this.settingsPanel.Controls.Add(this.goalsTrackBar);
+            this.settingsPanel.Controls.Add(this.rounds2Label);
+            this.settingsPanel.Controls.Add(this.roundsLabel);
+            this.settingsPanel.Controls.Add(this.roundsTrackBar);
+            this.settingsPanel.Controls.Add(this.cancelLabel);
+            this.settingsPanel.Controls.Add(this.saveLabel);
+            this.settingsPanel.Location = new System.Drawing.Point(13, 13);
+            this.settingsPanel.Name = "settingsPanel";
+            this.settingsPanel.Size = new System.Drawing.Size(709, 436);
+            this.settingsPanel.TabIndex = 5;
+            // 
+            // rounds2Label
+            // 
+            this.rounds2Label.AutoSize = true;
+            this.rounds2Label.BackColor = System.Drawing.Color.Black;
+            this.rounds2Label.ForeColor = System.Drawing.SystemColors.Window;
+            this.rounds2Label.Location = new System.Drawing.Point(221, 115);
+            this.rounds2Label.Name = "rounds2Label";
+            this.rounds2Label.Size = new System.Drawing.Size(98, 13);
+            this.rounds2Label.TabIndex = 6;
+            this.rounds2Label.Text = "Anzahl der Runden";
+            // 
+            // roundsLabel
+            // 
+            this.roundsLabel.AutoSize = true;
+            this.roundsLabel.BackColor = System.Drawing.Color.Black;
+            this.roundsLabel.ForeColor = System.Drawing.SystemColors.Window;
+            this.roundsLabel.Location = new System.Drawing.Point(511, 141);
+            this.roundsLabel.Name = "roundsLabel";
+            this.roundsLabel.Size = new System.Drawing.Size(13, 13);
+            this.roundsLabel.TabIndex = 5;
+            this.roundsLabel.Text = "1";
+            // 
+            // roundsTrackBar
+            // 
+            this.roundsTrackBar.Location = new System.Drawing.Point(215, 131);
+            this.roundsTrackBar.Maximum = 100;
+            this.roundsTrackBar.Minimum = 1;
+            this.roundsTrackBar.Name = "roundsTrackBar";
+            this.roundsTrackBar.Size = new System.Drawing.Size(290, 45);
+            this.roundsTrackBar.TabIndex = 4;
+            this.roundsTrackBar.Value = 1;
+            this.roundsTrackBar.Scroll += new System.EventHandler(this.roundsTrackBar_Scroll);
+            // 
+            // cancelLabel
+            // 
+            this.cancelLabel.AutoSize = true;
+            this.cancelLabel.BackColor = System.Drawing.Color.Black;
+            this.cancelLabel.ForeColor = System.Drawing.SystemColors.Window;
+            this.cancelLabel.Location = new System.Drawing.Point(28, 359);
+            this.cancelLabel.Name = "cancelLabel";
+            this.cancelLabel.Size = new System.Drawing.Size(59, 13);
+            this.cancelLabel.TabIndex = 3;
+            this.cancelLabel.Text = "Abbrechen";
+            this.cancelLabel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.cancelLabel_MouseClick);
+            // 
+            // saveLabel
+            // 
+            this.saveLabel.AutoSize = true;
+            this.saveLabel.BackColor = System.Drawing.Color.Black;
+            this.saveLabel.ForeColor = System.Drawing.SystemColors.Window;
+            this.saveLabel.Location = new System.Drawing.Point(625, 359);
+            this.saveLabel.Name = "saveLabel";
+            this.saveLabel.Size = new System.Drawing.Size(55, 13);
+            this.saveLabel.TabIndex = 2;
+            this.saveLabel.Text = "Speichern";
+            this.saveLabel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.saveLabel_MouseClick);
+            // 
+            // settings2Label
+            // 
+            this.settings2Label.AutoSize = true;
+            this.settings2Label.BackColor = System.Drawing.Color.Transparent;
+            this.settings2Label.Font = new System.Drawing.Font("Niagara Solid", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.settings2Label.ForeColor = System.Drawing.Color.White;
+            this.settings2Label.Location = new System.Drawing.Point(317, 223);
+            this.settings2Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.settings2Label.Name = "settings2Label";
+            this.settings2Label.Size = new System.Drawing.Size(118, 51);
+            this.settings2Label.TabIndex = 6;
+            this.settings2Label.Text = "Settings";
+            this.settings2Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.settings2Label.UseMnemonic = false;
+            this.settings2Label.MouseClick += new System.Windows.Forms.MouseEventHandler(this.settings2Label_MouseClick);
+            // 
+            // goals2Label
+            // 
+            this.goals2Label.AutoSize = true;
+            this.goals2Label.BackColor = System.Drawing.Color.Black;
+            this.goals2Label.ForeColor = System.Drawing.SystemColors.Window;
+            this.goals2Label.Location = new System.Drawing.Point(221, 38);
+            this.goals2Label.Name = "goals2Label";
+            this.goals2Label.Size = new System.Drawing.Size(82, 13);
+            this.goals2Label.TabIndex = 9;
+            this.goals2Label.Text = "Anzahl der Tore";
+            // 
+            // goalsLabel
+            // 
+            this.goalsLabel.AutoSize = true;
+            this.goalsLabel.BackColor = System.Drawing.Color.Black;
+            this.goalsLabel.ForeColor = System.Drawing.SystemColors.Window;
+            this.goalsLabel.Location = new System.Drawing.Point(511, 64);
+            this.goalsLabel.Name = "goalsLabel";
+            this.goalsLabel.Size = new System.Drawing.Size(13, 13);
+            this.goalsLabel.TabIndex = 8;
+            this.goalsLabel.Text = "1";
+            // 
+            // goalsTrackBar
+            // 
+            this.goalsTrackBar.Location = new System.Drawing.Point(215, 54);
+            this.goalsTrackBar.Minimum = 1;
+            this.goalsTrackBar.Name = "goalsTrackBar";
+            this.goalsTrackBar.Size = new System.Drawing.Size(290, 45);
+            this.goalsTrackBar.TabIndex = 7;
+            this.goalsTrackBar.Value = 1;
+            this.goalsTrackBar.Scroll += new System.EventHandler(this.goalsTrackBar_Scroll);
+            // 
             // Pong
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(734, 461);
+            this.Controls.Add(this.settings2Label);
+            this.Controls.Add(this.settingsPanel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ExitLabel);
             this.Controls.Add(this.SettingsLabel);
@@ -94,6 +235,10 @@
             this.Name = "Pong";
             this.Text = "PONG";
             this.Load += new System.EventHandler(this.Pong_Load);
+            this.settingsPanel.ResumeLayout(false);
+            this.settingsPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.roundsTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.goalsTrackBar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,6 +250,16 @@
         private System.Windows.Forms.Label SettingsLabel;
         private System.Windows.Forms.Label ExitLabel;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel settingsPanel;
+        private System.Windows.Forms.Label rounds2Label;
+        private System.Windows.Forms.Label roundsLabel;
+        private System.Windows.Forms.TrackBar roundsTrackBar;
+        private System.Windows.Forms.Label cancelLabel;
+        private System.Windows.Forms.Label saveLabel;
+        private System.Windows.Forms.Label settings2Label;
+        private System.Windows.Forms.Label goals2Label;
+        private System.Windows.Forms.Label goalsLabel;
+        private System.Windows.Forms.TrackBar goalsTrackBar;
     }
 }
 
